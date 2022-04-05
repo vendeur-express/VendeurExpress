4b06f5af450cb48f60e765f25caaf265c9131b63
AdamaKeleguemm <adamsouedsr@gmail.com>
2022-04-05 20:01:47 +0200

    Footer

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,117 @@
+<head>
+	<style>
+	.panel-footer.panel-custom {
+		background: #000000;
+		color: white;
+	}
+	</style>
+    <meta charset="UTF-8">
+    <title>Vendeur Express</title>
+    <link rel="manifest" href="assets/images/icons/site.html">
+    <meta name="apple-mobile-web-app-title" content="Molla">
+    <meta name="application-name" content="Molla">
+    <meta name="msapplication-TileColor" content="#cc9966">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet" href="assets/vendor/line-awesome/line-awesome/line-awesome/css/line-awesome.min.css">
+    <!-- Plugins CSS File -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/owl-carousel/owl.carousel.css">
+    <link rel="stylesheet" href="assets/css/plugins/magnific-popup/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/plugins/jquery.countdown.css">
+    <!-- Main CSS File -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/skins/skin-demo-4.css">
+    <link rel="stylesheet" href="assets/css/demos/demo-4.css">
+</head>
+
+
+
+
+
+
+
+
+
+
+<footer class="footer panel-footer panel-custom">
+        	<div class="footer-middle">
+	            <div class="container">
+	            	<div class="row">
+	            		<div class="col-sm-4 col-lg-3">
+	            			<div class="widget widget-about">
+	            				<img src="assets/images/logo.png" class="footer-logo" alt="Footer Logo" width="105" height="50">
+                                <ul class="widget-list">
+	            					<li><a href="#">Devenir un Vendeur Express</a></li>
+	            					<li><a href="#">Devenir un Demarcheur</a></li>
+	            				</ul>
+	            			</div><!-- End .widget about-widget -->
+	            		</div><!-- End .col-sm-6 col-lg-3 -->
+
+	            		<div class="col-sm-4 col-lg-2">
+	            			<div class="widget">
+	            				<h4 class="widget-title">Nos Formulaires</h4><!-- End .widget-title -->
+
+	            				<ul class="widget-list">
+	            					<li><a href="#">Devenir un Vendeur Express</a></li>
+	            					<li><a href="#">Devenir un Demarcheur</a></li>
+	            				</ul><!-- End .widget-list -->
+	            			</div><!-- End .widget -->
+	            		</div><!-- End .col-sm-6 col-lg-3 -->
+
+	            		<div class="col-sm-4 col-lg-2">
+	            			<div class="widget">
+	            				<h4 class="widget-title">Informations utiles</h4><!-- End .widget-title -->
+
+	            				<ul class="widget-list">
+	            					<li><a href="#">A Propos</a></li>
+	            					<li><a href="#">VEX SHOP</a></li>
+	            					<li><a href="#">Contactez-nous</a></li>
+	            					<li><a href="#">Livraison & Expédition</a></li>
+	            					<li><a href="#">Actualités</a></li>
+	            				</ul><!-- End .widget-list -->
+	            			</div><!-- End .widget -->
+	            		</div><!-- End .col-sm-6 col-lg-3 -->
+
+	            		<div class="col-sm-4 col-lg-2">
+	            			<div class="widget">
+	            				<h4 class="widget-title">Nos Services</h4><!-- End .widget-title -->
+
+	            				<ul class="widget-list">
+	            					<li><a href="#">Politique de confidentialité</a></li>
+	            					<li><a href="#">Condition d'utilisation</a></li>
+	            					<li><a href="#">Srvice Clientèle</a></li>
+	            					<li><a href="#">Informations de Livraison</a></li>
+	            					<li><a href="#">Paiements</a></li>
+	            				</ul><!-- End .widget-list -->
+	            			</div><!-- End .widget -->
+	            		</div><!-- End .col-sm-6 col-lg-3 -->
+                        <div class="col-sm-4 col-lg-2">
+	            			<div class="widget">
+	            				<h4 class="widget-title">NEWSLETTER</h4><!-- End .widget-title -->
+
+	            				<ul class="widget-list">
+	            					<li><a href="#">Abonnez-vous à notre newsletter</a></li>
+	            					<li><a href="#">Condition d'utilisation</a></li>
+	            					<li><a href="#">Srvice Clientèle</a></li>
+	            					<li><a href="#">Informations de Livraison</a></li>
+	            					<li><a href="#">Paiements</a></li>
+	            				</ul><!-- End .widget-list -->
+	            			</div><!-- End .widget -->
+	            		</div><!-- End .col-sm-6 col-lg-3 -->
+	            	</div><!-- End .row -->
+	            </div><!-- End .container -->
+	        </div><!-- End .footer-middle -->
+
+	        <div class="footer-bottom" backgroundcolor="red">
+	        	<div class="container">
+                <p class="footer-copyright"> ©
+                    <script>
+                        document.write(new Date().getFullYear());
+                    </script>
+	        		VENDEUR EXPRESS</p><!-- End .footer-copyright -->
+	        		<figure class="footer-payments">
+	        			<img src="assets/images/payments.png" alt="Payment methods" width="272" height="20">
+	        		</figure><!-- End .footer-payments -->
+	        	</div><!-- End .container -->
+	        </div><!-- End .footer-bottom -->
+        </footer><!-- End .footer -->
---
+++ resources/views/include/clientfooter.blade.php
@@ -0,0 +1,25 @@
+<footer>
+            <div class="footer-bottom">
+                <div class="container">
+                    
+                </div><!-- End .container -->
+            </div><!-- End .footer-bottom -->
+        	<div class="footer-middle">
+	            <div class="container">
+
+	            </div><!-- End .container -->
+	        </div><!-- End .footer-middle -->
+
+	        <div class="footer-bottom" backgroundcolor="red">
+	        	<div class="container">
+                <p class="footer-copyright"> ©
+                    <script>
+                        document.write(new Date().getFullYear());
+                    </script>
+	        		VENDEUR EXPRESS</p><!-- End .footer-copyright -->
+	        		<figure class="footer-payments">
+	        			<img src="assets/images/payments.png" alt="Payment methods" width="272" height="20">
+	        		</figure><!-- End .footer-payments -->
+	        	</div><!-- End .container -->
+	        </div><!-- End .footer-bottom -->
+        </footer><!-- End .footer -->
