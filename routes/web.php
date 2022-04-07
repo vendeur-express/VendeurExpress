4d17da4a06b0449d44a757bfe2448490eb6f1ee4
zcelestin <celestinzongo95@gmail.com>
2022-04-07 11:23:04 +0000

    first navbar
    modification du navbar

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('navbar');
 })->name('acceuil');
 // Route::get('/', function () {
 //     return view('category');
