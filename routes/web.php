ce29173668975c82d11fe2f694970f53421bacce
AdamaKeleguemm <adamsouedsr@gmail.com>
2022-04-09 15:17:41 +0200

    route change

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/acceuil', function () {
     return view('navbar');
 })->name('acceuil');
 
@@ -21,6 +21,6 @@ Route::get('/home', function(){
     return view('dashboard');
 })->name('home');
 
-Route::get('/client',function(){
+Route::get('/',function(){
     return view('client');
 });
