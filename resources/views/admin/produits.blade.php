217c149847641c6fc058fa104678c71d626a6e65
AdamaKeleguemm <adamsouedsr@gmail.com>
2022-04-22 20:06:07 +0200

    22042022 rembourser & commandes screens

--- resources/views/admin/produits.blade.php
+++ resources/views/admin/produits.blade.php
@@ -13,7 +13,7 @@
     PRODUITS | VENDEUR EXPRESS
 @endsection
 @section('contenu')
-<div class="content-wrapper">
+  <div class="content-wrapper">
     <!-- Main content -->
     <section class="content">
         <br>
@@ -268,42 +268,42 @@
         </div>
         <!-- /.container-fluid -->
       </section>
-      <!-- /.content -->
-</div>
-@endsection
-<!-- Bootstrap 4 -->
-<script src="admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- DataTables  & Plugins -->
-<script src="admin/plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="admin/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-<script src="admin/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-<script src="admin/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-<script src="admin/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-<script src="admin/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-<script src="admin/plugins/jszip/jszip.min.js"></script>
-<script src="admin/plugins/pdfmake/pdfmake.min.js"></script>
-<script src="admin/plugins/pdfmake/vfs_fonts.js"></script>
-<script src="admin/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-<script src="admin/plugins/datatables-buttons/js/buttons.print.min.js"></script>
-<script src="admin/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
-<!-- AdminLTE App -->
-<script src="admin/dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="admin/dist/js/demo.js"></script>
-<script>
-    $(function () {
-      $("#example1").DataTable({
-        "responsive": true, "lengthChange": false, "autoWidth": false,
-        "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-      }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-      $('#example2').DataTable({
-        "paging": true,
-        "lengthChange": false,
-        "searching": false,
-        "ordering": true,
-        "info": true,
-        "autoWidth": false,
-        "responsive": true,
+    <!-- /.content -->
+  </div>
+  !-- Bootstrap 4 -->
+  <script src="admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- DataTables  & Plugins -->
+  <script src="admin/plugins/datatables/jquery.dataTables.min.js"></script>
+  <script src="admin/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+  <script src="admin/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+  <script src="admin/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+  <script src="admin/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+  <script src="admin/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+  <script src="admin/plugins/jszip/jszip.min.js"></script>
+  <script src="admin/plugins/pdfmake/pdfmake.min.js"></script>
+  <script src="admin/plugins/pdfmake/vfs_fonts.js"></script>
+  <script src="admin/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+  <script src="admin/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+  <script src="admin/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="admin/dist/js/adminlte.min.js"></script>
+  <!-- AdminLTE for demo purposes -->
+  <script src="admin/dist/js/demo.js"></script>
+  <script>
+      $(function () {
+        $("#example1").DataTable({
+          "responsive": true, "lengthChange": false, "autoWidth": false,
+          "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+        $('#example2').DataTable({
+          "paging": true,
+          "lengthChange": false,
+          "searching": false,
+          "ordering": true,
+          "info": true,
+          "autoWidth": false,
+          "responsive": true,
+        });
       });
-    });
-  </script>
+    </script>
+@endsection
--- resources/views/admin/rembourser.blade.php
+++ resources/views/admin/rembourser.blade.php
@@ -4,6 +4,58 @@
 @endsection
 @section('contenu')
 <div class="content-wrapper">
-    <h1>rembourser page</h1>
+    <section class="content">
+        <br>
+        <div class="container-fluid">
+            <div class="card">
+                <div class="card-body">
+                    <h2>
+                        Demandes de remboursement
+                    </h2>
+                </div> 
+            </div>
+        </div>
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-12">
+              <div class="card">
+                <!-- /.card-header -->
+                <div class="card-body">
+                  <table id="example1" class="table table-bordered table-striped">
+                    <thead>
+                    <tr>
+                      <th>ID Commande</th>
+                      <th>Magasin</th>
+                      <th>Montant</th>
+                      <th>Type</th>
+                      <th>Raison</th>
+                      <th>Date</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <tr></tr>
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <th>ID Commande</th>
+                            <th>Magasin</th>
+                            <th>Montant</th>
+                            <th>Type</th>
+                            <th>Raison</th>
+                            <th>Date</th>
+                          </tr>
+                    </tfoot>
+                  </table>
+                </div>
+                <!-- /.card-body -->
+              </div>
+              <!-- /.card -->
+            </div>
+            <!-- /.col -->
+          </div>
+          <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+      </section>
 </div>
 @endsection
